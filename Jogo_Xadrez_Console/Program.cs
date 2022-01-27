507b62540fad2d7ac3be0692cacdab189fb8a465
Nelio Alves <gabriel.junio@rocketmail.com>
2022-01-27 01:55:35 -0300

    Criando a classe Posicao

--- Jogo_Xadrez_Console/Program.cs
+++ Jogo_Xadrez_Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace Jogo_Xadrez_Console
 {
@@ -6,7 +7,7 @@ namespace Jogo_Xadrez_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
